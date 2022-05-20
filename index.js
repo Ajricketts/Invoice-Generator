7a9b8fb72996a7b4b4ad5f76af30c7c28510de23
AJ Ricketts <rickettsaj1@gmail.com>
2022-05-19 21:56:51 -0400

    Finished overall functionallity and added comments

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
+// Dictionary of all the services and their prices
 const services = {
     "Wash Car": "$10",
     "Mow Lawn": "$20",
     "Pull Weeds": "$30"
 }
 
+// DOM interaction, grabbing all the elements needed
 const washBtn = document.getElementsByClassName("btn")[0]
 const mowBtn = document.getElementsByClassName("btn")[1]
 const pullBtn = document.getElementsByClassName("btn")[2]
 const taskDiv = document.getElementsByClassName("task")[0]
 const totalDiv = document.getElementsByClassName("total")[0]
+const sendBtn = document.getElementsByClassName("send-inv")[0]
+
+// Boolean values to track whether a given service has been added to the invoice already (dont want to add services twice)
 let washAdded = false
 let mowAdded = false
 let pullAdded = false
 
+// Render the Car Wash service in the invoice
 washBtn.addEventListener("click", function(){
     if (!washAdded) {
         addTask(Object.keys(services)[0]) 
@@ -20,6 +26,7 @@ washBtn.addEventListener("click", function(){
     }
 })
 
+// Render the Lawn Mowing service in the invoice
 mowBtn.addEventListener("click", function(){
     if (!mowAdded) {
         addTask(Object.keys(services)[1]) 
@@ -27,6 +34,7 @@ mowBtn.addEventListener("click", function(){
     }
 })
 
+// Render the Pulling Weeds service in the invoice
 pullBtn.addEventListener("click", function(){
     if (!pullAdded) {
         addTask(Object.keys(services)[2]) 
@@ -34,18 +42,38 @@ pullBtn.addEventListener("click", function(){
     }
 })
 
+// Open the users default mail client so they can send the invoice
+// TODO add a formated invoice attatchment in the email
+sendBtn.addEventListener("click", function(){
+    window.open('mailto:test@example.com')
+})
+
+//Add the service to the invoice with a remove button and correct price
 function addTask(key) {
     taskDiv.innerHTML += `
         <div class="desc">
             <p>${key}</p>
-            <button class="remove">Remove</button>
+            <button class="remove" onclick="removeTask(this, '${key}')">Remove</button>
         </div>
     `
     totalDiv.innerHTML += `
-        <p class="price">${services[key]}</p>
+        <p class="price" id="${key}">${services[key]}</p>
     `
 }
 
-function removeTask(parent) {
-    
+// Remove the sercive from the invoice along with the price
+function removeTask(elem, dictKey) {
+    let parentDiv = elem.parentNode
+    console.log(parentDiv)
+    parentDiv.remove()
+    document.getElementById(dictKey).remove()
+    if (dictKey == "Wash Car") {
+        washAdded = false
+    }
+    else if (dictKey =="Mow Lawn"){
+        mowAdded = false
+    }
+    else {
+        pullAdded = false
+    }
 }
